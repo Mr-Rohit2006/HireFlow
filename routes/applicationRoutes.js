3788fbc1cfcf9051d4fc2fb8b32d9e1a9ebbb747
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2026-02-11 00:08:30 +0530

    Update applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../middleware/upload");
 const isAuth = require("../middleware/isAuth");
-const isStudent = require("../middleware/isStudent");
+const {
+  applyJob,
+  myApplications,
+} = require("../controllers/applicationController");
 
-// 👇 controller poora object le lo
-const applicationController = require("../controllers/applicationController");
-
-// APPLY JOB (POST – with resume)
 router.post(
   "/apply/:jobId",
   isAuth,
-  isStudent,
-  applicationController.applyJob
+  upload.single("resume"), // 🔥 NAME MUST MATCH FORM
+  applyJob
 );
 
-// MY APPLICATIONS
-router.get(
-  "/my-applications",
-  isAuth,
-  isStudent,
-  applicationController.myApplications
-);
+router.get("/my-applications", isAuth, myApplications);
 
 module.exports = router;
