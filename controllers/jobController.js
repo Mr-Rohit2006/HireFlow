c9e8c9f213b3e6c86bc5ac60495382f6f7a6cb55
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2026-02-11 00:25:17 +0530

    Update jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,23 +1,34 @@
 const Job = require("../models/Job");
 
 exports.getJobs = async (req, res) => {
-  const jobs = await Job.find();
-  res.render("jobs/jobs", { jobs });
+  try {
+    const jobs = await Job.find();
+    res.render("jobs/jobs", { jobs });
+  } catch (err) {
+    console.error("Get jobs error:", err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 exports.postJob = async (req, res) => {
-  if (!req.session.user) {
-  return res.redirect("/login");
-  }
-  await Job.create({
-    title: req.body.title,
-    company: req.body.company,
-    location: req.body.location,
-    salary: req.body.salary,
-    jobType: req.body.jobType,
-    description: req.body.description,
-    recruiter: req.session.user._id
-  });
+  try {
+    if (!req.session.user) {
+      return res.redirect("/login");
+    }
 
-  res.redirect("/recruiter/dashboard");
+    await Job.create({
+      title: req.body.title,
+      company: req.body.company,
+      location: req.body.location,
+      salary: req.body.salary,
+      jobType: req.body.jobType,
+      description: req.body.description,
+      recruiter: req.session.user._id,
+    });
+
+    res.redirect("/recruiter/dashboard");
+  } catch (err) {
+    console.error("Post job error:", err);
+    res.status(500).send("Error posting job");
+  }
 };
