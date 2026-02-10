c110aa2d14fa19a8aacde3e52e6e4102659eea0d
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2026-02-11 00:08:51 +0530

    Update applicationController.js

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,13 +1,18 @@
-const Application = require("../models/Application"); // 🔥 MISSING LINE FIXED
+const Application = require("../models/Application");
 
 exports.applyJob = async (req, res) => {
   try {
     const jobId = req.params.jobId;
     const studentId = req.session.user._id;
 
+    // 🔥 MOST IMPORTANT SAFETY CHECK
+    if (!req.file) {
+      return res.status(400).send("Resume not uploaded");
+    }
+
     const alreadyApplied = await Application.findOne({
       job: jobId,
-      student: studentId
+      student: studentId,
     });
 
     if (alreadyApplied) {
@@ -18,20 +23,20 @@ exports.applyJob = async (req, res) => {
       job: jobId,
       student: studentId,
       resume: req.file.path,
-      status: "Pending"
+      status: "Pending",
     });
 
     res.redirect("/my-applications");
   } catch (err) {
-    console.error(err);
+    console.error("Apply job error:", err);
     res.send("Error applying job");
   }
 };
 
-
 exports.myApplications = async (req, res) => {
   const applications = await Application.find({
-    student: req.session.user._id
+    student: req.session.user._id,
   }).populate("job");
 
-res.render("applications/applications", { applications });};
+  res.render("applications/applications", { applications });
+};
